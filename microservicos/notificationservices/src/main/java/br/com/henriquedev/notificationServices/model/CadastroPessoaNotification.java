3781dcb41af2a0f7ed555b6fea959d11eaed1e24
Esdras Henrique <esdrasccs@hotmail.com>
2025-02-28 20:13:12 -0300

    fix: Corrigindo model CadastroPessoaNotification, removendo parametro lombook que estava ocasionando o nao builder da aplicação.

--- microservicos/notificationservices/src/main/java/br/com/henriquedev/notificationServices/model/CadastroPessoaNotification.java
+++ microservicos/notificationservices/src/main/java/br/com/henriquedev/notificationServices/model/CadastroPessoaNotification.java
@@ -1,10 +1,9 @@
 package br.com.henriquedev.notificationServices.model;
 
-import lombok.AllArgsConstructor;
+
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
 public class CadastroPessoaNotification {
     private String nome;
     private String email;
