91b5d2ba855c505ff21f2335e3210af9e165fac5
Esdras Henrique <esdrasccs@hotmail.com>
2025-02-28 17:49:27 -0300

    feat: Inicio do microserviço publicher cadastro service.

---
+++ microservicos/cadastroservices/src/main/java/br/com/henriquedev/CadastroServices/CadastroServicesApplication.java
@@ -0,0 +1,13 @@
+package br.com.henriquedev.CadastroServices;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CadastroServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CadastroServicesApplication.class, args);
+	}
+
+}
